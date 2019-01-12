b153f4c6df3b342b0c83b1389c8e890fc35b78b5
Shivam Verma <shivamv2392@gmail.com>
2019-01-12 13:13:40 -0700

    Adding login and signup functionalities

--- myproject/myproject/accounts/views.py
+++ myproject/myproject/accounts/views.py
@@ -1,23 +1,22 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
 from django.contrib.auth import login as auth_login
 
 # Create your views here.
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm()
-        args ={
-            'form': form
-        }
+        form = SignUpForm(request.POST)
         if form.is_valid():
             user = form.save()
-            print(user)
             auth_login(request, user)
             return redirect('home')
-        return render(request, 'signup.html', args)
+        return render(request, 'signup.html', {'form':form})
     else:
-        form = UserCreationForm()
+        form = SignUpForm()
         args ={
             'form': form
         }
         return render(request, 'signup.html', args)
+
+def login(request):
+    return render(request, 'login.html')
--- myproject/myproject/myproject/settings.py
+++ myproject/myproject/myproject/settings.py
@@ -16,6 +16,9 @@ import config
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+LOGIN_REDIRECT = 'home'
+LOGOUT_REDIRECT_URL = 'home'
+LOGIN_URL = 'login'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -143,3 +146,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
